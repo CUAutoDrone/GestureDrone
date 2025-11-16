0552dca02b9b520fb85e5c42a9b0a9540232621f
Rohan Shankar <rohan.p.shankar@gmail.com>
2025-11-16 19:30:49 +0000

    Merging help

---
+++ build/gesture_to_flight/CMakeFiles/gesture_to_flight_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gesture_to_flight_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/gesture_to_flight_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/gesture_to_flight/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/Downloads/GestureDrone/gesture_to_flight
+# Build directory: /home/ubuntu/Downloads/GestureDrone/build/gesture_to_flight
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
