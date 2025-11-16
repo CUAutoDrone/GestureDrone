10277282f331f14752b14936ea8a534afb60b705
Rohan Shankar <rohan.p.shankar@gmail.com>
2025-11-16 19:30:49 +0000

    Merging help

---
+++ build/gesture_to_flight/CMakeFiles/flight_instructions.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flight_instructions.dir/src/flight_instructions.cpp.o"
+  "CMakeFiles/flight_instructions.dir/src/flight_instructions.cpp.o.d"
+  "flight_instructions"
+  "flight_instructions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flight_instructions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/gesture_to_flight/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: GestureDrone/gesture_to_flight
+# Build directory: GestureDrone/build/gesture_to_flight
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
